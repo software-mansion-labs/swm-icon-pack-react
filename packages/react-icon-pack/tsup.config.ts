ce0a121424b1a605d945f8b870dbd1fb4f3e3fc8
alduzy <alduzy@gmail.com>
2024-04-17 14:39:02 +0200

    moved build options to config file

--- packages/react-icon-pack/tsup.config.ts
+++ packages/react-icon-pack/tsup.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'tsup'
 import svgr from 'esbuild-plugin-svgr'
 
 export default defineConfig({
+  format: ['cjs', 'esm'],
+  dts: true,
   esbuildPlugins: [
     svgr({
       replaceAttrValues: { 'black': 'currentColor' }
