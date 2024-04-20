a792d61d4fc5a135e737afa005b876afccdc8d9b
Igor Furgala <exploif@icloud.com>
2024-04-20 20:25:01 +0200

    fix fonts convertion

--- cli.js
+++ cli.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs'
-import {hideBin} from 'yargs/helpers'
-import svgtofont from 'svgtofont'
-import fs from 'fs'
-import path from 'path'
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+import svgtofont from 'svgtofont';
+import fs from 'fs';
+import path from 'path';
 
 const argv = yargs(hideBin(process.argv))
   .option('source', {
     alias: 's',
     describe: 'Provide path to svg source',
-    type: 'string',
+    type: 'string'
   })
   .option('output', {
     alias: 'o',
     describe: 'Provide font output directory',
-    type: 'string',
+    type: 'string'
   })
   .option('fontName', {
     alias: 'f',
     describe: 'Provide font name',
-    type: 'string',
-  }).argv
+    type: 'string'
+  }).argv;
 
-const sourcesPath = path.resolve(process.cwd(), argv.source)
-const outputPath = path.resolve(process.cwd(), argv.output)
-const glyphMap = {}
+const sourcesPath = path.resolve(process.cwd(), argv.source);
+const outputPath = path.resolve(process.cwd(), argv.output);
+const glyphMap = {};
 
 svgtofont({
   src: sourcesPath,
@@ -36,36 +36,43 @@ svgtofont({
   outSVGReact: false,
   outSVGReactNative: false,
   outSVGPath: false,
+  generateInfoData: true,
   svgicons2svgfont: {
     fontHeight: 1000,
-    normalize: true,
-  },
-  getIconUnicode: (iconName, _unicode, charValue) => {
-    glyphMap[iconName] = charValue
-
-    return [iconName, charValue]
-  },
+    normalize: true
+  }
 })
   .then(() => {
-    fs.writeFileSync(`${outputPath}/glyphmap.json`, JSON.stringify(glyphMap))
+    fs.readFile(path.join(outputPath, 'info.json'), { encoding: 'utf-8' }, (err, data) => {
+      if (err) throw err;
 
-    console.log('Font created')
+      const infoJson = JSON.parse(data);
+      for (const iconName in infoJson) {
+        const unicodeString = infoJson[iconName].unicode.replace(/[^0-9.]/g, '');
+        glyphMap[iconName] = Number.parseInt(unicodeString);
+      }
+
+      fs.writeFileSync(`${outputPath}/glyphmap.json`, JSON.stringify(glyphMap));
+    });
+
+    console.log('Font created');
   })
   .catch(err => {
-    console.warn('Error while creating fonts:', err)
+    console.log('Error while creating fonts:', err);
   })
   .finally(() => {
     // svgtofont creates other font files automatically, which is not configurable
     // lines below should remove them completely
     fs.readdir(outputPath, (err, files) => {
-      if (err) throw err
+      if (err) throw err;
 
       files.forEach(file => {
-        if (file.endsWith('.json') || file.endsWith('.ttf')) return
+        if (file === 'glyphmap.json' || file.endsWith('.ttf')) return;
 
         fs.unlink(path.join(outputPath, file), err => {
-          if (err) console.warn('Error while deleting unused file', err)
-        })
-      })
-    })
-  })
+          if (err) console.warn('Error while deleting unused file', err);
+        });
+      });
+    });
+  });
+
